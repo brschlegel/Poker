acd964f1e05feeed967281e4a99613de694ee598
Ben Schlegel <brschlegels@gmail.com>
2020-05-10 18:13:09 -0400

    Basic Setup

---
+++ Cards.py
@@ -0,0 +1,27 @@
+class Card:
+    nextCard = None
+    rank= 0
+    suit = ""
+    
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    ##overriding tostring
+    def __str__(self):
+        ##python doesn't have switch statements :(
+        if self.rank < 11:
+            return str(self.rank) + self.suit
+        if self.rank == 11:
+            return "J" + self.suit
+        if self.rank == 12:
+            return "Q" + self.suit
+        if self.rank == 13:
+            return "K" + self.suit
+        if self.rank == 14:
+            return "A" + self.suit
+        
+clubCodes = {"Clubs":"\u2663", "Hearts":"\u2665", "Diamonds":"\u2666", "Spades":"\u2660"}
+
+
+print(Card(13, clubCodes["Diamonds"]))
